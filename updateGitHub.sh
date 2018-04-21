535031fadbbc2c7234cdbc00a63ae2e9689b3452
625343614 <625343614@qq.com>
2018-04-21 12:01:35 +0800

    1

---
+++ updateGitHub.sh
@@ -0,0 +1,21 @@
+
+#! /bin/bash 
+# 保存当前目录 
+# currentDir=$PWD 
+# echo "Start to publish...\n" 
+# # 切换到项目目录 
+# cd /usr/local/nginx/html/blog 
+# # 执行git命令 
+# git pull origin test 
+# git push origin master 
+# 切换回原来的目录 
+#cd $currentDir 
+# 删除缓存 
+#rm -rvf /usr/local/nginx/html/blog/runtime/cache echo "Success\n";
+
+
+# 切换到项目目录 
+cd /home/wwwroot/html/inbcn88 
+# 执行git命令 
+git pull  
+#git push origin master 
